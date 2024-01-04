8112570253b6c66c3b82015ec86f74684f37a40d
Mayank <mail.mayank001@gmail.com>
2024-01-04 19:51:32 +0530

    First commit

--- STL/04_Iterator.cpp
+++ STL/04_Iterator.cpp
@@ -4,10 +4,16 @@ using namespace std;
 // We use iterator for accesing the elements in the vector.
 void iterators()
 {
-    vector<int> v; // Initialise the v.
+    vector<int> v;                        // Initialise the v.
     vector<int>::iterator it = v.begin(); // v.begin() :- It points to the memory address not on the element.
+    it++;
+    cout << *it << " ";
 
 
+
+
+
+    
 
     vector<int> arr = {1, 2, 3, 4, 3};
     vector<int>::iterator ptr;
