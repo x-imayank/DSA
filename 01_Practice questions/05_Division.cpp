6f52216f0109eee8030ac711e0d8e4f459adf09f
Mayank <mail.mayank001@gmail.com>
2024-01-08 01:11:16 +0530

    First commit

--- 01_Practice questions/05_Division.cpp
+++ 01_Practice questions/05_Division.cpp
@@ -1,34 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void division(int n)
-{
-    vector<int> ls;
-    for (int i = 1; i <= sqrt(n); i++)
-    {
-        if (n % i == 0)
-        {
-            ls.push_back(i);
-            if ((n / i) != i)
-            {
-                ls.push_back(n / i);
-            }
-        }
-    }
-    sort(ls.begin(), ls.end());
-    for (auto it : ls)
-    {
-        cout << ls << " ";
-    }
-}
+// void division(int n)
+// {
+//     for (int i = 1; i <= sqrt(n); i++)
+//     {
+//         if (n % i == 0)
+//         {
+//             cout << i << " ";
+//             if ((n / i) != i)
+//             {
+//                 cout << n / i << " ";
+//             }
+//         }
+        
+//     }
+// }
 
-int main()
-{
-    int n;
-    cin >> n;
-    division(n);
-}
-
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     division(n);
+// }
 
 // void division(int n)
 // {
---
+++ 04_Recurssion/01_Print-name.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void name(int i, int n)
+{
+    if (i > n)
+    {
+        return;
+    }
+    cout << "Mayank" << endl;
+
+    name(i + 1, n); // i incremenent until it is not greater than n;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    name(1, 5); // i starts from 1 and increase upto 5
+}
