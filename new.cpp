f4164cca0015737a41bbaec49e6056bba69d7854
Mayank <mail.mayank001@gmail.com>
2024-03-30 22:20:46 +0530

    First commit

--- new.cpp
+++ new.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    int sum = 0;
-    for (int i = 0; i <= n; i++)
+    int a, i = 1, sum = 0;
+    cout << "Enter the value of a: ";
+    cin >> a;
+    while (i <= a)
     {
-        sum += i;
+
+        sum = sum + i;
+        i = i + 1;
     }
+
     cout << sum;
 }
